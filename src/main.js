c26e2a8bcc49e2d004d68c06c894b0446675e90b
AzizxonZufarov <91425039+AzizxonZufarov@users.noreply.github.com>
2023-02-26 14:59:41 +0500

    9th lesson completed

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import messagePlugin from '@/utils/message.plugin'
+import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+import Loader from '@/components/app/Loader'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+Vue.config.productionTip = false
+
+
+Vue.use(messagePlugin)
+Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.use(Vuelidate)
+Vue.component('Loader', Loader)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCTg8_-1JMJl5rHKZLAys-BcU3z29Cm2N0",
+  authDomain: "vue-crm-5817d.firebaseapp.com",
+  databaseURL: "https://vue-crm-5817d-default-rtdb.firebaseio.com",
+  projectId: "vue-crm-5817d",
+  storageBucket: "vue-crm-5817d.appspot.com",
+  messagingSenderId: "1003652266825",
+  appId: "1:1003652266825:web:c2e6222b4dbfcd8da43898"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
+
---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import auth from './auth'
+import info from './info'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    error: null
+  },
+  mutations: {
+    setError(state, error) {
+      state.error = error
+    },
+    clearError(state) {
+      state.error = null
+    }
+  },
+  actions: {
+    async fetchCurrency() {
+      const key = process.env.VUE_APP_FIXER
+
+
+      const res = await fetch(`https://anyapi.io/api/v1/exchange/rates?apiKey=${key}`)
+      console.log(res);
+      return await res.json()
+    }
+  },
+  getters: {
+    error: s => s.error
+  },
+  modules: {
+    auth, info
+  }
+})
